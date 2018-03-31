804cac5ccc054feb62562001a999abbdc86f98fb
Arnold MESSIFET <am@idfr.net>
2018-03-31 11:50:59 +0200

    update Dialogs page to use platform-choice component

--- src/pages/dialogs/dialogs.module.ts
+++ src/pages/dialogs/dialogs.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DialogsPage } from './dialogs';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { DialogsPage } from './dialogs';
   ],
   imports: [
     IonicPageModule.forChild(DialogsPage),
+    ComponentsModule
   ],
 })
 export class DialogsPageModule {}
--- src/pages/dialogs/dialogs.ts
+++ src/pages/dialogs/dialogs.ts
@@ -1,43 +1,27 @@
 import { Component } from '@angular/core';
-import { AlertController, IonicPage, Platform } from 'ionic-angular';
+import { AlertController, IonicPage } from 'ionic-angular';
 import { Dialogs } from '@ionic-native/dialogs';
 
-@IonicPage()
+@IonicPage({
+  defaultHistory: ['HomePage']
+})
 @Component({
   selector: 'page-dialogs',
   templateUrl: 'dialogs.html',
 })
 export class DialogsPage {
-  isCordovaDisplayed = false;
-  isIonicDisplayed = false;
+  public plugin = 'Dialogs';
+  public iconName = 'chatboxes';
+  public isCordovaDisplayed = false;
+  public isIonicDisplayed = false;
 
   constructor(private dialogs: Dialogs,
-              private platform: Platform,
               private alertCtrl: AlertController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DialogsPage');
-  }
-
-  displayIonicFeactures(): void {
-    this.isCordovaDisplayed = false;
-    this.isIonicDisplayed = true;
-  }
-
-  displayCordovaFeatures(): void {
-    if (this.platform.is('cordova') && this.platform.is('mobile')) {
-      this.isCordovaDisplayed = true;
-      this.isIonicDisplayed = false;
-    } else {
-      this.alertCtrl
-        .create({
-          title: 'Dialogs plugin is not available on this device',
-          buttons: ['OK']
-        })
-        .present();
-    }
-
+  setChoice(event: {isIonicSelected: boolean, isCordovaSelected: boolean}): void {
+    this.isIonicDisplayed = event.isIonicSelected;
+    this.isCordovaDisplayed = event.isCordovaSelected;
   }
 
   alertCordova():void {
@@ -51,11 +35,13 @@ export class DialogsPage {
       .then(() => console.log('Dialog dismissed'))
       .catch(e => console.log('Error displaying dialog', e));
   }
+
   promptCordova():void {
     this.dialogs.prompt('What\'s the weather today ?')
       .then(() => console.log('Dialog dismissed'))
       .catch(e => console.log('Error displaying dialog', e));
   }
+
   beepCordova():void {
     this.dialogs.beep(3);
   }
@@ -69,6 +55,41 @@ export class DialogsPage {
       .present();
   }
 
+  confirmIonic(): void {
+    this.alertCtrl
+      .create({
+        title: 'Hello world',
+        message: 'Do you confirm ?',
+        buttons: [
+          {
+            text: 'Disagree',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+          },
+          {
+            text: 'Agree',
+            handler: () => {
+              console.log('Agree clicked');
+            }
+          }
+        ]
+      })
+      .present();
+  }
 
-
+  promptIonic(): void {
+    this.alertCtrl
+      .create({
+        title: 'Hello world',
+        inputs: [
+          {
+            name: 'What\'s the weather today ?',
+            placeholder: 'Title'
+          },
+        ],
+        buttons: ['OK']
+      })
+      .present();
+  }
 }
